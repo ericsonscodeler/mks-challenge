768ddc3e5421e3e26114ace515d7d4cd1a6cb411
Ericson Scodeler <ericson.br@hotmail.com>
2023-11-06 11:51:28 -0300

    fix font montserrat

--- src/app/globalStyles.ts
+++ src/app/globalStyles.ts
@@ -5,6 +5,7 @@ const GlobalStyles = createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
+     font-family: 'Montserrat', sans-serif;
   }
 `
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 import Provider from '../utils/providers'
-const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'MKS Challenge',
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={montserrat.className}>
         <Provider>{children}</Provider>
       </body>
     </html>
